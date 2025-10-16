a6ef81480a0eb8e44cd83dd85cf65fae5a5c1b3c
Artikko <05arthurpadua@gmail.com>
2025-10-15 21:49:28 -0300

    Configuraçao banco de dados e da tabela de alimentos

---
+++ Projeto_BackEnd/Models/Alimento.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_BackEnd.Models
+{
+    [Table("Alimentos")]
+    public class Alimento
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatorio informar o Alimento!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatorio informar o Tipo do Alimento!")]
+        public string Tipo { get; set; }
+    }
+}
--- Projeto_BackEnd/Program.cs
+++ Projeto_BackEnd/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Projeto_BackEnd.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
